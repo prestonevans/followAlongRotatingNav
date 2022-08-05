cc55d9157bf4202cec5726adacf9169522838855
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-05 16:33:52 -0600

    meh

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const open = document.querySelector('#open')
+const close = document.querySelector('#close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click',() => {
+    container.classList.add('show-nav')
+})
+
+close.addEventListener('click',() => {
+    container.classList.remove('show-nav')
+})
